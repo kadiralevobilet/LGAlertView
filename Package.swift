4ae498b5776039fae29a62d32f22e49a19c73b13
Kadir Kutluhan Alev <kadir.alev@obilet.com>
2024-08-12 11:35:17 +0300

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "LGAlertView",
+    platforms: [
+        .iOS(.v13) // Minimum iOS
+    ],
+    products: [
+        .library(
+            name: "LGAlertView",
+            targets: ["LGAlertView"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "LGAlertView",
+            dependencies: [],
+            path: "./LGAlertView/", // Objective-C source file path
+            publicHeadersPath: "./Framework/", // Header file path
+            cSettings: [
+                .headerSearchPath("./Framework/") // Header search path
+            ]
+        ),
+        .testTarget(
+            name: "ProjeAdiTests",
+            dependencies: ["ProjeAdi"],
+            path: "Tests"
+        ),
+    ]
+)
+
